703353240c1804fb7ca19c72ac3995389e0df16f
Goumang <goumang2010@live.com>
2015-12-26 17:09:02 +0800

    Extract fileInfo method to my toolkit

--- FileManagerNew/MainPage.cs
+++ FileManagerNew/MainPage.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using Microsoft.Office.Interop.Word;
+using GoumangToolKit;
 namespace FileManagerNew
 {
 
